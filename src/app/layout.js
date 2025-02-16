3735bbdc533314c70e86cc1aa4d1a83e3e67ad30
JAY PRAKASH <JAYPRAKASH93347@GMAIL.COM>
2025-02-16 20:29:36 +0530

    Made seo frriendly

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,15 +14,63 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Mozi",
+  title: "Mozi YouTube Player",
   description:
-    "Mozi is a modern, minimal, and fast personal YouTube player via links.",
+    "Mozi is a modern, minimal, and fast personal YouTube player via links. Enjoy a clean interface to play your favorite YouTube videos with ease.",
+  keywords: [
+    "Mozi",
+    "YouTube Player",
+    "personal YouTube player",
+    "modern YouTube player",
+    "minimal YouTube player",
+    "fast YouTube player",
+    "responsive YouTube player",
+    "open source YouTube player",
+    "video player",
+    "moze github",
+    "minimal design",
+    "modern design",
+    "Vercel deployment",
+    "I am Jay Prakash",
+    "moze video player",
+    "youtube video player",
+    "personal video player",
+    "clean interface",
+    "fast video player",
+  ],
+  openGraph: {
+    title: "Mozi YouTube Player",
+    description:
+      "Mozi is a modern, minimal, and fast personal YouTube player via links. Enjoy a clean interface to play your favorite YouTube videos with ease.",
+    url: "https://moze-seven.vercel.app/",
+    type: "website",
+    images: [
+      {
+        url: "https://moze-seven.vercel.app/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Mozi YouTube Player",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Mozi YouTube Player",
+    description:
+      "Mozi is a modern, minimal, and fast personal YouTube player via links. Enjoy a clean interface to play your favorite YouTube videos with ease.",
+    images: ["https://moze-seven.vercel.app/twitter-image.png"],
+  },
+  alternates: {
+    canonical: "https://moze-seven.vercel.app/",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <Navbar />
         {children}
         <Footer />
